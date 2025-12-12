25cf0ec9d1d6152c8d481c58e952ad791a223a79
Daniel Semblano <dsemblano@gmail.com>
2025-12-12 14:56:55 -0300

    site changed to vercel temporary

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://tourmacapa.com.br',
+	site: 'https://lucianotigre-astro.vercel.app/',
 	integrations: [
 		mdx(),
 		sitemap(),
