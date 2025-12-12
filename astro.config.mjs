2611a88d650392e0ca166771ca01fd08590bb02b
Daniel Semblano <dsemblano@gmail.com>
2025-12-12 12:04:06 -0300

    subdomain removed

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import { remarkModifiedTime } from './src/plugins/remark-modified-time.mjs'
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://tourmacapa.com.br',
-	base: '/saral-theme-astro',
+	output: 'static',
 	integrations: [
 		mdx(),
 		sitemap(),
